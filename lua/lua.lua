40db15252ccf6e81c84651d1d55dbdd370db4956
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2024-12-15 18:29:32 +0300

    Update lua.lua

--- lua/lua.lua
+++ lua/lua.lua
@@ -1,4 +1,4 @@
---[Horizontal Scrolling]--
+---[Horizontal Scrolling]---
 local Conteiner = vgui.Create("DHorizontalScroller", frame5)
     Conteiner:SetSize(913, 67)
     Conteiner:SetPos(42, 498)
@@ -9,3 +9,19 @@ end
 function Conteiner.btnRight:Paint( w, h )
     draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 100, 200,0) )
 end
+
+
+---[]---
+--[HTML]--
+
+if (window.gmod{ window.gmod.emit('button_click', 'button1');}
+
+--[LUA]--
+
+html:AddFunction("gmod", "emit", function(buttonId)
+   if type(buttonId) == "string" then
+   print("Кнопка нажата: " .. buttonId)
+   else
+      print("Получен неожиданный тип: " .. type(buttonId))
+   end
+end)
